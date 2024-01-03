fe0c5f04d535649227b647c2a24b4a9c05d3ca99
ChloeL-E <chloe_c123@hotmail.co.uk>
2024-01-03 09:53:44 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const companies = [
+{name: 'Company 1', category: 'finance', start: 1981, end: 2003},
+{name: 'Company 2', category: 'retail', start: 1992 , end: 2008},
+{name: 'Company 3', category: 'auto', start: 1999, end: 2007},
+{name: 'Company 4', category: 'retail', start: 1989, end: 2010},
+{name: 'Company 5', category: 'tech', start: 2009,  end: 2014},
+{name: 'Company 6', category: 'finance', start: 1987, end: 2010},
+{name: 'Company 7', category: 'auto', start: 1986, end: 1996},
+{name: 'Company 8', category: 'tech', start: 2011, end: 2016},
+{name: 'Company 9', category: 'retail', start: 1981, end: 1989}
+];
+
+const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+
+for(let i = 0; i < companies.length; i++){
+console.log(companies[i]);
+};
+
